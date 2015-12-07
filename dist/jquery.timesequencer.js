97aed92b2be51a2d11fa31492b41fc3b872e54ad
john <jwayling@gmail.com>
2015-12-07 22:55:35 +1000

    Added dist and package.json

---
+++ dist/jquery.timesequencer.js
@@ -0,0 +1,20 @@
+(function ( $ ) {
+    $.fn.timesequence = function( options ) {
+
+        var settings = $.extend({
+            // These are the defaults.
+            length: 60 //seconds
+
+        }, options );
+
+        var container = this;
+
+
+
+
+
+
+
+
+    };
+}( jQuery ));
